0da39785037f9de099a9bd22731ffe864c99e652
pratik pageni <pagenipratik@gmail.com>
2024-06-28 23:32:15 +0545

    feat: add Product model, controller, and index

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Management</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-gray-100">
+    <div class="container mx-auto p-4">
+        @yield('content')
+    </div>
+</body>
+</html>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+<h1 class="text-2xl font-bold mb-4">Products</h1>
+@endsection
